28dd9db7ec60bc4d5c9ac9edfdafc198f47c373e
adelyser <bluwolf@gmail.com>
2020-10-14 09:26:08 -0600

    Update __init__.py
    change update url to my own

--- octoprint_m73etaoverride/__init__.py
+++ octoprint_m73etaoverride/__init__.py
@@ -25,7 +25,7 @@ class M73ETA(octoprint.plugin.OctoPrintPlugin,octoprint.plugin.RestartNeedingPlu
             user="sysadminsh",
             repo="OctoPrint-M73ETAOverride",
 
-            pip="https://github.com/sysadminsh/OctoPrint-M73ETAOverride/archive/{target}.zip"
+            pip="https://github.com/adelyser/OctoPrint-M73ETAOverride/archive/{target}.zip"
         )
     )
 
